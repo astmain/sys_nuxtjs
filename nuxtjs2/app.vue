6ddb03c050c1bdadbe6b83a6667f5f40cf7c61af
astmain <1311192345@qq.com>
2025-09-08 00:20:16 +0800

    1

---
+++ nuxtjs2/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+
+
+
+
+
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+
+  </div>
+</template>
+
+
+
+<script>
+
+
+console.log('app.vue');
+</script>
